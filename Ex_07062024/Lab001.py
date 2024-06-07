4d3268a1e5950a6b5a44d2092f75a863f0ba0e96
shubham-sutar <shubhamsutar93@gmail.com>
2024-06-07 10:28:15 +0530

    added

---
+++ Ex_07062024/Lab001.py
@@ -0,0 +1,38 @@
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+result = int(num1) + int(num2)
+print("addition: ", result)
+
+pi = 3.14
+print(pi)
+print(type(pi))
+
+i = 10
+i2 = 12
+result = i2/i
+print(result)
+
+# strings
+# Bunch of Char
+
+# Raw String
+dir = r'c:\nomedir\some dir'
+print(dir)
+
+# Format of the string
+fname = "shubham"
+lname = "sutar"
+
+print(f"{fname} {lname}")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ex_07062024/Lab002.py
@@ -0,0 +1,19 @@
+val = None
+# val = val + 1 unsupport
+# Data Type None
+print(val)
+
+name = None
+print(name)
+name = "shubham"
+print(name)
+print(id(name))
+
+# List
+# add items
+# remove items
+# update
+
+#
+
+
